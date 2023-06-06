1d7aa91767c6c07dacef7933e24fc40dd4ecc314
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-06-06 10:30:48 -0600

    dd

---
+++ resource-definition/sqs.tf
@@ -0,0 +1,41 @@
+resource "humanitec_resource_definition" "sqs" {
+  driver_type = "${var.humanitec_organization}/terraform"
+  id          = "${local.app}-sqs"
+  name        = "${local.app}-sqs"
+  type        = "postgres"
+
+  driver_inputs = {
+    secrets = {
+      variables = jsonencode({
+
+      })
+    },
+    values = {
+      "source" = jsonencode(
+        {
+          path = "terraform/sqs/"
+          rev  = "refs/heads/main"
+          url  = "https://github.com/nickhumanitec/humanitec-coprovisioning-test.git"
+        }
+      )
+      "variables" = jsonencode(
+        {
+          app = "$${context.app.id}"
+          env = "$${context.env.id}"
+          res = "$${context.res.id}"
+        }
+      )
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      criteria
+    ]
+  }
+
+}
+
+resource "humanitec_resource_definition_criteria" "sqs" {
+  resource_definition_id = humanitec_resource_definition.sqs.id
+  app_id                 = humanitec_application.app.id
+}
